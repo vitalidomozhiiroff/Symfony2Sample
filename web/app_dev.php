13d0c99e54ce4c9026049e3bad94a0704cca7418
Vitali D <vitali@emagineers.co.uk>
2013-12-26 19:56:16 +0000

    First Day

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,12 +3,14 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+//phpinfo();
+
+// çou don't want to setup permissions the proper way, just uncomment the following PHP lineine
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
+// Feel free to remove this, extend it, or make something more sophisticated.d.d.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
@@ -17,13 +19,13 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-Debug::enable();
-
+// $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/autoload.php';
 require_once __DIR__.'/../app/AppKernel.php';
 
+
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+// $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
